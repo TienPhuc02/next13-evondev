98cf488a68ae492e2f5dc95f0372126e908e6314
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-10-05 17:04:08 +0700

    commit last

--- src/app/property/[slug]/page.tsx
+++ src/app/property/[slug]/page.tsx
@@ -1,6 +1,10 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 const Page = ({ params }: { params: { slug: string } }) => {
+  // cách lấy slug từ URl
+  const  router =useRouter()
+  console.log("🚀 ~ file: page.tsx:7 ~ Page ~ router:", router)
   return (
     <>
       slug property
